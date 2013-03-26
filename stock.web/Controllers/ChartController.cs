7e510f3f4223be478c6c7ec8fa8505b4d5ee5971
Cosmin Duta <noir2501@gmail.com>
2013-03-26 21:19:19 +0200

    Added list with ajax functionality.

---
+++ stock.web/Controllers/ChartController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using ClosedXML.Excel;
+using stock.web.Infrastructure;
+
+namespace stock.web.Controllers
+{
+    public class ChartController : Controller
+    {
+        //
+        // GET: /Chart/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonNetResult GetStocksData(string sheet, string xcolumn, string ycolumn)
+        {
+            string path = Server.MapPath("~/stockdata.xlsx");
+            var wb = new XLWorkbook(path, XLEventTracking.Disabled);
+            var ws = wb.Worksheet(sheet);
+
+            var firstRow = ws.FirstRowUsed();
+            var row = firstRow.RowBelow();
+
+            var result = new List<dynamic>();
+
+            while (!row.Cell(xcolumn).IsEmpty() || !row.Cell(ycolumn).IsEmpty())
+            {
+                var x = row.Cell(xcolumn).Value;
+                var y = row.Cell(ycolumn).Value;
+
+                result.Add(new { x = x, y = y });
+                row = row.RowBelow();
+            }
+
+            return new JsonNetResult(result);
+        }
+
+    }
+}
---
+++ stock.web/Controllers/ListController.cs
@@ -0,0 +1,43 @@
+﻿using stock.web.Infrastructure;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace stock.web.Controllers
+{
+    public class ListController : Controller
+    {
+        //
+        // GET: /List/
+
+        public ActionResult Index()
+        {
+
+            var Names = new List<string>
+                            {
+                                "Aiden",
+                                "Jackson",
+                                "Ethan",
+                                "Liam",
+                                "Mason",
+                                "Noah",
+                                "Lucas",
+                                "Jacob",
+                                "Jayden",
+                                "Jack",
+                                "Logan",
+                                "Ryan",
+                                "Caleb",
+                                "Benjamin"
+                            };
+        
+            return View(Names);
+        }
+
+        [HttpPost]
+        public JsonNetResult Greetings(string name)
+        {
+            return new JsonNetResult(string.Format("Hello {0}!", name));
+        }
+
+    }
+}
