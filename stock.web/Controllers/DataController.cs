59d8b839c4a6e3f1a06b39ca7d5efc6377d994ec
Cosmin Duta <noir2501@gmail.com>
2013-03-27 16:10:13 +0200

    Created partial views sample with database.

---
+++ stock.web/Controllers/DataController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+using stock.web.Models;
+
+namespace stock.web.Controllers
+{
+    public class DataController : Controller
+    {
+        private const string connectionString = "Server=localhost;Database=world;Uid=manager;Pwd=123letmein";
+
+        public ActionResult Index()
+        {
+            List<City> cities = new List<City>();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception)
+                {
+                    return View();
+                }
+
+                MySqlCommand command = new MySqlCommand("SELECT * FROM City LIMIT 0, 50", connection);
+
+                var reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                    while (reader.Read())
+                    {
+                        City city = new City
+                                        {
+                                            ID = Convert.ToInt32(reader["ID"]),
+                                            Name = reader["Name"] as string,
+                                            CountryCode = reader["CountryCode"] as string,
+                                            District = reader["District"] as string,
+                                            Population = Convert.ToUInt32(reader["population"])
+                                        };
+
+                        cities.Add(city);
+                    }
+
+                connection.Close();
+            }
+
+            return View(cities);
+        }
+
+    }
+}
---
+++ stock.web/Models/City.cs
@@ -0,0 +1,11 @@
+﻿namespace stock.web.Models
+{
+    public class City
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string CountryCode { get; set; }
+        public string District { get; set; }
+        public uint Population { get; set; }
+    }
+}
